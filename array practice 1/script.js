608a97207a12246f0cec39f08814ffb27e76ed7b
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-07-10 13:07:32 +0545

    practice

---
+++ array practice 1/script.js
@@ -0,0 +1,42 @@
+// const reversedNum = (arr) => {
+//   let reversed = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     reversed.push(arr[i]);
+//   }
+//   return reversed;
+// };
+// let array = [2, 4, 5, 1, 3];
+// let reversedResult = reversedNum(array);
+// console.log(reversedResult);
+
+//reversing a string
+
+// const reversedString = (str) => {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed = reversed + str[i];
+//   }
+//   return reversed;
+// };
+// let string = "monkey";
+// let reversedResult = reversedString(string);
+// console.log(reversedResult);
+
+//print evennumbers from an array
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// let evenNumber = numbers.filter((num) => num % 2 == 0);
+// console.log(evenNumber);
+
+// function returns the sum of the sqrt of numbers
+
+// const sumOfSquare = (arr) => {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum = sum + Math.sqrt(arr[i]);
+//   }
+//   return sum;
+// };
+// let array = [4, 9, 16, 25, 36];
+// let resultSum = sumOfSquare(array);
+// console.log(resultSum);
