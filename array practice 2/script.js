8a6bfecab1e8982291762dab71e6e752e2d0accf
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-07-12 14:52:47 +0545

    practice

---
+++ array practice 2/script.js
@@ -0,0 +1,32 @@
+//Given an arrray and target, return indices of  two numbers in array, such that they add upto target.
+//They may assume that, each input would have exactly one solution,  and you may not use the same elements twice. You may return the answer in any order.
+// Eg. nums=[2, 7, 11,15], target; 9 =>[0, 1]
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+const targetArray = (arr) => {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        newArray.push(i, j);
+      }
+    }
+  }
+  return newArray;
+};
+let result = targetArray(nums);
+console.log(result);
+
+// Reverse the array using unshift function
+
+const reverseArray = (arr) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.unshift(arr[i]);
+  }
+  return newArr;
+};
+let numbers = [1, 2, 3, 4, 5];
+let reversedResult = reverseArray(numbers);
+console.log(reversedResult);
