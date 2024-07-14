26c6de285286cf4b9024e41c992b85b00cc9f43b
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-07-14 20:54:16 +0545

    practice

--- Array practice 3/script.js
+++ Array practice 3/script.js
@@ -1,39 +1,87 @@
-// Reverse num
+// // Reverse num
+// // const reverseNum = (num) => {
+// //   for (let i = num; i > 0; i--) {
+// //     console.log(i);
+// //   }
+// // };
+// // reverseNum(5);
+
+// print each reversed num
+
 // const reverseNum = (num) => {
-//   for (let i = num; i > 0; i--) {
-//     console.log(i);
+//   let numString = num.toString();
+//   let reversed = "";
+//   for (let i = numString.length - 1; i >= 0; i--) {
+//     reversed = reversed + numString[i];
+//   }
+//   for (let j = 0; j < reversed.length; j++) {
+//     console.log(reversed[j]);
 //   }
 // };
-// reverseNum(5);
+// reverseNum(2468);
 
-// Reverse numbers
-function reverseNum(num) {
-  let reversed = "";
-  let numString = String(num);
-  //   console.log(numString);
-  for (let i = numString.length - 1; i >= 0; i--) {
-    // console.log(i);
-    reversed = reversed + numString[i];
-    // console.log(reversed);
-  }
-  return reversed;
-}
-let result = reverseNum(8792);
-console.log(result);
+// // Reverse numbers
+// function reverseNum(num) {
+//   let reversed = "";
+//   let numString = String(num);
+//
+//   for (let i = numString.length - 1; i >= 0; i--) {
+//
+//     reversed = reversed + numString[i];
+//
+//   }
+//   return reversed;
+// }
+// let result = reverseNum(8792);
+// console.log(result);
+
+// // OR
+
+// let numbers = 45678;
+// const backwardNum = (num) => {
+//   let numString = num.toString();
+//   let reverse = "";
+//   for (let i = numString.length; i > 0; i--) {
+//     let remainder = num % 10;
+//     // console.log(reminder);
+//     num = parseInt(num / 10);
+//     reverse = reverse + remainder;
+//   }
+//   return reverse;
+// };
+// let output = backwardNum(numbers);
+// console.log(output);
+
+// Practice about split in string
+
+// let text = "How are you doing?";
+// let newText = text.split("");
+// console.log(newText[1 ]);
+
+// let text = "How are you doing?";
+// let newText = text.split("o");
+// console.log(newText);
+
+// let text = "How are you doing?";
+// let newText = text.split();
+// console.log(newText);
+
+// Reverse the number using function (split, reverse, join)
+
+// const reverseName = (str) => {
+//   let reversedText = str.split("");
+//   console.log(reversedText);
+//   let reversed = [];
+//   for (let i = reversedText.length - 1; i >= 0; i--) {
+//     console.log(reversedText[i]);
+//     reversed.push(reversedText[i]);
+//   }
+//   console.log(reversed.join(""));
+// };
+// let name = "Bandhu";
+// reverseName(name);
 
 // OR
 
-let numbers = 45678;
-const backwardNum = (num) => {
-  let numString = num.toString();
-  let reverse = "";
-  for (let i = numString.length; i > 0; i--) {
-    let reminder = num % 10;
-    // console.log(reminder);
-    num = parseInt(num / 10);
-    reverse = reverse + reminder;
-  }
-  return reverse;
-};
-let output = backwardNum(numbers);
-console.log(output);
+// let name = "Bandhu";
+// console.log(name.split("").reverse().join(""));
