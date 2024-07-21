26adaa477bff6967ce72deaa3ad5da765556b472
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-07-21 14:43:17 +0545

    practice of map and for of

--- 21.7.2024/script.js
+++ 21.7.2024/script.js
@@ -1,3 +1,6 @@
+// Q.Give an array of numbers, create a new array where each element is the square of
+// the original number using the 'map()' function.
+
 // for loop//
 
 // const squareArray = (arr) => {
@@ -26,3 +29,45 @@
 // };
 // let result = squareArray([2, 4, 5, 6, 8]);
 // console.log(result);
+
+// map //
+
+// let array = [2, 3, 4, 6, 8];
+// let newArr = array.map((ele) => ele * ele);
+// let result = newArr;
+// console.log(result);
+
+// Q. Write a function that an array of strings an returns a new
+// array with the lengths of each string using the 'map()' function.
+
+// const lengthStrings = (arr) => {
+//   let newArr = [];
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     let stringLength = arr[i].length;
+//     newArr.push(stringLength);
+//   }
+//   return newArr;
+// };
+// let names = ["Bipin", "Bandhu", "Shiva", "Lekhnath"];
+// let result = lengthStrings(names);
+// console.log(result);
+
+// for of//
+
+// const lengthStrings = (arr) => {
+//   let newArr = [];
+//   for (let name of arr) {
+//     let stringLength = name.length;
+//     newArr.push(stringLength);
+//   }
+//   return newArr;
+// };
+// let names = ["Bipin", "Bandhu", "Shiva", "Lekhnath"];
+// let result = lengthStrings(names);
+// console.log(result);
+
+// map //
+
+// let names = ["Bipin", "Bandhu", "Shiva", "Lekhnath"];
+// let lengthOfstring = names.map((name) => name.length);
+// console.log(lengthOfstring);
