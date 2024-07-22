2bfb01d0e624b25f895a455ebe4a749370c39fef
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-07-22 17:08:47 +0545

    practice

---
+++ 22.7.2024/script.js
@@ -0,0 +1,34 @@
+// Q. write a function that takes an array of strings and return a
+// new array with all the strings converted first letter of is capitalized.
+
+// const firstLetterUpperCase = (arr) => {
+//   let newArr = [];
+//   for (let str of arr) {
+//     let upperCasedStr = str.charAt(0).toUpperCase() + str.slice(1);
+//     // console.log(upperCasedStr);
+//     newArr.push(upperCasedStr);
+//   }
+//   return newArr;
+// };
+// let names = ["man", "cow", "people", "police"];
+// let result = firstLetterUpperCase(names);
+// console.log(result);
+
+// let names = ["man", "cow", "people", "police"];
+// let upperCasedStr = names.map(function (str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// });
+// console.log(upperCasedStr);
+
+// object //
+
+// let myinfo = {
+//   name: "Bandhu",
+//   village: "Karmchour",
+//   district: "Syangja",
+// };
+// myinfo.village = "Rampur";
+// myinfo.city = "KTM";
+// for (let key in myinfo) {
+//   console.log(myinfo[key]);
+// }
