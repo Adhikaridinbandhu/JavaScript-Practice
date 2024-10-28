fb0709e7a3691fdab5f143b57436b94a36308fd0
Din Bandhu Adhikari <adbandhu23@gmail.com>
2024-10-29 00:03:45 +0545

    further commit

--- node/controllers/places-controllers.js
+++ node/controllers/places-controllers.js
@@ -69,6 +69,11 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs passed, Please check your data!", 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
--- node/routes/places-routes.js
+++ node/routes/places-routes.js
@@ -13,12 +13,17 @@ router.post(
   "/",
   [
     check("title").not().isEmpty(),
-    check("description").isLength({ min: 5 }, check("address").not().isEmpty()),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
   ],
   placesControllers.createPlace
 );
 
-router.patch("/:pid", placesControllers.updatePlace);
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placesControllers.updatePlace
+);
 
 router.delete("/:pid", placesControllers.deletePlace);
 
